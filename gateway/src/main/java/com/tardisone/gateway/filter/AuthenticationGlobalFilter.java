d74806ce54eae70175be8734c7829bb93f61f3a8
zhaojian <zhaojian@easternbay.cn>
2019-02-18 18:15:06 +0800

    create authentication service

---
+++ gateway/src/main/java/com/tardisone/gateway/filter/AuthenticationGlobalFilter.java
@@ -0,0 +1,22 @@
+package com.tardisone.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthenticationGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.err.println("In Authentication filter！");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -2147482649;
+    }
+}
