470a3f705a0f0c30cfe342ed107d7b2c824eebf3
zhaojian <zhaojian@easternbay.cn>
2019-02-18 11:47:36 +0800

    initiate project

---
+++ company-service/src/main/java/com/tardisone/companyservice/config/RestConfiguration.java
@@ -0,0 +1,16 @@
+package com.tardisone.companyservice.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
