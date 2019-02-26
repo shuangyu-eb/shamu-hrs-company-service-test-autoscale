eb583b849c8036e5d8e5a754f56ee6237e07bc1f
JiaQi1007 <jiaqi@easternbay.cn>
2019-02-26 11:55:44 +0800

    remove bolumer

--- web/javascript/App/Home/NavBar/index.jsx
+++ web/javascript/App/Home/NavBar/index.jsx
@@ -17,50 +17,50 @@ class NavBar extends React.Component {
 
   render() {
     return (
-      <Navbar role="navigation" aria-label="main navigation">
-        <NavbarBrand>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
           <Link className="navbar-item logo" to="/">
             <img src={logo} alt="logo" />
           </Link>
-        </NavbarBrand>
+        </div>
 
-        <NavbarMenu>
-          <NavbarStart>
-            <NavbarItem>
-              <Field>
-                <Control hasIcons="right">
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <div className="navbar-item">
+              <div className="field">
+                <div className="control has-icons-right">
                   <input className="input is-medium search-input" onChange={this.onSearCh} type="text" placeholder="Search employees & documents" />
                   <span className="icon is-small is-right">
                     <img src={search} alt="search" />
                   </span>
-                </Control>
-              </Field>
-            </NavbarItem>
-          </NavbarStart>
+                </div>
+              </div>
+            </div>
+          </div>
 
-          <NavbarEnd>
-            <NavbarItem>
+          <div className="navbar-end">
+            <div className="navbar-item">
               <span className="icon is-large">
                 <img src={notifications} alt="notifications" className="notifications" />
               </span>
-            </NavbarItem>
-            <NavbarItem>
-              <Dropdown>
-                <DropdownTrigger>
-                  <Button isOutlined className="profile-picture" aria-haspopup="true" aria-controls="dropdown-menu">
+            </div>
+            <div className="navbar-item">
+              <div className="dropdown">
+                <div className="dropdown-trigger">
+                  <button type="button" className="button profile-picture" aria-haspopup="true" aria-controls="dropdown-menu">
                     <span>picture</span>
-                  </Button>
-                </DropdownTrigger>
-                <DropdownMenu>
-                  <DropdownContent>
-                    <DropdownItem href="#">First item</DropdownItem>
-                  </DropdownContent>
-                </DropdownMenu>
-              </Dropdown>
-            </NavbarItem>
-          </NavbarEnd>
-        </NavbarMenu>
-      </Navbar>
+                  </button>
+                </div>
+                <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                  <div className="dropdown-content">
+                    <button type="button" className="dropdown-item">First item</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
