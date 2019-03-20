91d5dbf7dcd3689b426c995120c774e3443c220c
zhaojian <zhaojian@easternbay.cn>
2019-03-20 10:18:48 +0800

    jira/SH-44 Integrate Sendgrid Into Hrs System

--- src/main/java/com/tardisone/companyservice/service/AuthUserService.java
+++ src/main/java/com/tardisone/companyservice/service/AuthUserService.java
@@ -3,5 +3,5 @@ package com.tardisone.companyservice.service;
 import com.tardisone.companyservice.entity.User;
 
 public interface AuthUserService {
-    public User getUser();
+    User getUser();
 }
--- src/main/java/com/tardisone/companyservice/service/SendEmailService.java
+++
@@ -1,8 +0,0 @@
-package com.tardisone.companyservice.service;
-
-import com.tardisone.companyservice.entity.User;
-import com.tardisone.companyservice.pojo.Email;
-
-public interface SendEmailService {
-    Boolean inviteEmployee(User employee, User currentUser, Email email);
-}
