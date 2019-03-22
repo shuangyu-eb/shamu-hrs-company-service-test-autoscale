29d8206871375417238b83da9b29b4b0ef18b764
JiaQi1007 <jiaqi@easternbay.cn>
2019-03-22 14:53:19 +0800

    jira/SH-41: test for qa

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DOCKER_LOGIN=`aws ecr get-login --region ap-northeast-1`
+DOCKER_LOGIN=`aws ecr get-login --no-include-email --region ap-northeast-1`
 ${DOCKER_LOGIN}
 
 mvn clean package -Dmaven.test.skip=true -P dev -e
