6daf4fc1e70af8d73942dc219aaaf1ff0b1c2f28
zhaojian <zhaojian@easternbay.cn>
2019-03-22 14:04:00 +0800

    jira/SH-15 Employer Registration Signup Page

--- src/main/java/com/tardisone/companyservice/controller/user/UserRestController.java
+++ src/main/java/com/tardisone/companyservice/controller/user/UserRestController.java
@@ -1,6 +1,5 @@
 package com.tardisone.companyservice.controller.user;
 
-import com.tardisone.companyservice.pojo.Email;
 import com.tardisone.companyservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -15,8 +14,8 @@ public class UserRestController {
     UserService userService;
 
     @PostMapping(value = "user/register/email")
-    public HttpEntity sendVerifyEmail(@RequestBody Email email) {
-        userService.sendVerifyEmail(email.getTo());
+    public HttpEntity sendVerifyEmail(@RequestBody String email) {
+        userService.sendVerifyEmail(email);
         return new ResponseEntity(HttpStatus.OK);
     }
 }
