5dd70baab797f9665702c6b69d1d7058ff0584e3
JiaQi1007 <jiaqi@easternbay.cn>
2019-03-26 10:51:02 +0800

    Jira/SH-41: optimize  deploy

--- deploy.sh
+++ deploy.sh
@@ -10,3 +10,6 @@ docker build -t company-service .
 docker tag company-service:latest 533423936407.dkr.ecr.ap-northeast-1.amazonaws.com/company-service:latest
 
 docker push 533423936407.dkr.ecr.ap-northeast-1.amazonaws.com/company-service:latest
+
+aws ecs update-service --cluster Services --region ap-northeast-1 --service company-service --task-definition company-service:4 --desired-count 1 --force-new-deployment
+
--- localdeploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-mvn clean
-mvn clean package -Dmaven.test.skip=true -Plocal -e
-docker build -t company-service .
-docker run -p 8081:8081 company-service --spring.profiles.active=local
