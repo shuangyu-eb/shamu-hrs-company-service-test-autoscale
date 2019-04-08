5f27e6f3be80f1cc1e5f85b49f014e2885ded25f
rex <geyiwei@easternbay.cn>
2019-04-08 17:17:27 +0800

    Create the State entity Modify the UserEmergencyContact.java

---
+++ src/main/java/com/tardisone/companyservice/entity/State.java
@@ -0,0 +1,16 @@
+package com.tardisone.companyservice.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "cities")
+@Where(clause = "deleted_at IS NULL")
+public class State extends BaseEntity {
+
+	private String name;
+}
