9e3e6f67cfc499741cbaa153b6738a79b84950f8
JiaQi1007 <jiaqi@easternbay.cn>
2019-04-12 11:45:27 +0800

    jira/SH-68: resolve the error caused by table corelate with each other

--- src/main/java/shamu/company/user/entity/User.java
+++ src/main/java/shamu/company/user/entity/User.java
@@ -1,20 +1,25 @@
 package shamu.company.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Where;
 import shamu.company.common.entity.BaseEntity;
 import shamu.company.company.entity.Company;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import java.sql.Timestamp;
 
 @Data
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
-//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 @Where(clause = "deleted_at IS NULL")
 public class User extends BaseEntity {
 
