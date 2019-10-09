603f63633a7dae4e7fc5953e1f72fcc7f4faf32e
Haibo Wang <48240833+haibo-eb@users.noreply.github.com>
2019-10-09 16:13:30 +0800

    659: Delete service interface in info module (#351)
    * delete service interface, rename impl
    
    * refactor
    
    * refactor

--- src/main/java/shamu/company/info/service/UserEmergencyContactService.java
+++ src/main/java/shamu/company/info/service/UserEmergencyContactService.java
@@ -1,15 +1,46 @@
 package shamu.company.info.service;
 
 import java.util.List;
+import org.springframework.stereotype.Service;
+import shamu.company.common.exception.ResourceNotFoundException;
 import shamu.company.info.entity.UserEmergencyContact;
+import shamu.company.info.repository.UserEmergencyContactRepository;
 
-public interface UserEmergencyContactService {
+@Service
+public class UserEmergencyContactService {
 
-  List<UserEmergencyContact> getUserEmergencyContacts(Long userId);
+  private final UserEmergencyContactRepository userEmergencyContactRepository;
 
-  void createUserEmergencyContact(Long userId, UserEmergencyContact userEmergencyContact);
+  public UserEmergencyContactService(
+      final UserEmergencyContactRepository userEmergencyContactRepository) {
+    this.userEmergencyContactRepository = userEmergencyContactRepository;
+  }
 
-  void deleteEmergencyContact(Long userId, Long id);
+  public List<UserEmergencyContact> getUserEmergencyContacts(final Long userId) {
+    return userEmergencyContactRepository.findByUserId(userId);
+  }
 
-  void updateEmergencyContact(Long userId, UserEmergencyContact emergencyContact);
+  public void createUserEmergencyContact(final Long userId,
+      final UserEmergencyContact userEmergencyContact) {
+    if (userEmergencyContact.getIsPrimary()) {
+      userEmergencyContactRepository.releasePrimaryContact(userId);
+    }
+    userEmergencyContactRepository.save(userEmergencyContact);
+  }
+
+  public void deleteEmergencyContact(final Long userId, final Long id) {
+    final UserEmergencyContact userEmergencyContact =
+        userEmergencyContactRepository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("UserEmergencyContact does not exist!"));
+    userEmergencyContactRepository.releasePrimaryContact(userId);
+    userEmergencyContactRepository.delete(id);
+    if (userEmergencyContact.getIsPrimary()) {
+      userEmergencyContactRepository.resetPrimaryContact(userId);
+    }
+  }
+
+  public void updateEmergencyContact(final Long userId,
+      final UserEmergencyContact userEmergencyContact) {
+    createUserEmergencyContact(userId, userEmergencyContact);
+  }
 }
