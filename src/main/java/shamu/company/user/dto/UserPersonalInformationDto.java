e1dd1b9316bd32a330e5839d66f2c301ec2dd0a6
Shuangyu Zhu <34657912+shuangyu-eb@users.noreply.github.com>
2019-11-15 04:02:07 +0800

    872: server side validation (#452)
     update

--- src/main/java/shamu/company/user/dto/UserPersonalInformationDto.java
+++ src/main/java/shamu/company/user/dto/UserPersonalInformationDto.java
@@ -1,5 +1,6 @@
 package shamu.company.user.dto;
 
+import javax.validation.constraints.Pattern;
 import lombok.Data;
 import shamu.company.crypto.Crypto;
 import shamu.company.hashids.HashidsFormat;
@@ -19,6 +20,10 @@ public class UserPersonalInformationDto extends BasicUserPersonalInformationDto 
   private String ethnicityName;
 
   @Crypto(field = "id", targetType = UserPersonalInformation.class)
+  @Pattern(
+      regexp = "^(?!00)(?!666)(?!9[0-9][0-9])\\d{3}[- ]?(?!00)\\d{2}[- ]?(?!0000)"
+          + "\\d{4}$",
+      message = "Your ssn doesn't meet our requirements.")
   private String ssn;
 
   private String imageUrl;
