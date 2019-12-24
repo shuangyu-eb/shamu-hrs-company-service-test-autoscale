608ed7f85a4b043fbb219c270e3eb44f362a024c
Shuangyu Zhu <34657912+shuangyu-eb@users.noreply.github.com>
2019-12-24 16:21:36 +0800

    1331：make consitent with v1 (#574)
    1331：make consitent with v1 (#574)

--- src/main/java/shamu/company/benefit/entity/DependentRelationship.java
+++ src/main/java/shamu/company/benefit/entity/DependentRelationship.java
@@ -3,11 +3,17 @@ package shamu.company.benefit.entity;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import shamu.company.common.entity.BaseEntity;
 
 @Data
 @Entity
 @Table(name = "benefit_plan_dependent_relationships")
+@NoArgsConstructor
 public class DependentRelationship extends BaseEntity {
   private String name;
+
+  public DependentRelationship(final String id) {
+    this.setId(id);
+  }
 }
