d3d3c0dc1aab395eb4b099cd044654b923ce11e1
Haibo Wang <48240833+haibo-eb@users.noreply.github.com>
2019-12-30 15:34:03 +0800

    1237: SonarQube helpers/s3/AwsHelper (#600)
    Resolve sonarlint tips

--- src/main/java/shamu/company/helpers/s3/AwsHelper.java
+++ src/main/java/shamu/company/helpers/s3/AwsHelper.java
@@ -13,6 +13,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.Calendar;
 import java.util.UUID;
 import javax.validation.constraints.NotNull;
@@ -29,15 +30,19 @@ import shamu.company.common.exception.response.ErrorType;
 @Slf4j
 public class AwsHelper {
 
-  private static final long expires = 1000 * 60 * 60 * 6;
+  private static final long EXPIRES = 1000 * 60 * 60 * 6L;
 
-  private static String tempUrl = "/temp";
+  private static final String TEMP_URL = "/temp";
 
-  private static String uploads = "/uploads";
+  private static final String UPLOADS = "/uploads";
 
-  private String bucketName;
+  private static final String FAILED_TO_UPLOAD_FILE = "Failed to upload file!";
 
-  private String folder;
+  private static final String ERROR_MESSAGE_FORMAT = "Error Message:    %s";
+
+  private final String bucketName;
+
+  private final String folder;
 
   @Autowired
   public AwsHelper(@Value("${aws.bucketName}") final String bucketName,
@@ -53,7 +58,7 @@ public class AwsHelper {
   private File generateFile(final String originalFilename) {
     try {
       final String projectDir = ResourceUtils.getURL(ResourceUtils.CLASSPATH_URL_PREFIX).getPath();
-      final String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+      final String suffix = originalFilename.substring(originalFilename.lastIndexOf('.'));
 
       final String fileName = String.format("%suploads/%s%s", projectDir,
           System.currentTimeMillis(),
@@ -89,7 +94,7 @@ public class AwsHelper {
       multipartFile.transferTo(file);
     } catch (final IOException e) {
       log.error(String.format("Caught an exception while uploading file: %s", e.getMessage()));
-      throw new AwsException("Failed to upload file!");
+      throw new AwsException(FAILED_TO_UPLOAD_FILE);
     }
 
     return uploadFile(file.getPath(), type, accessType);
@@ -118,19 +123,15 @@ public class AwsHelper {
       log.error(
           "Caught an AmazonServiceException, which means your request made it to Amazon S3,"
               + " but was rejected with an error response for some reason.");
-      log.error(String.format("Error Message:    %s", ase.getMessage()));
+      log.error(String.format(ERROR_MESSAGE_FORMAT, ase.getMessage()));
       log.error(String.format("HTTP Status Code: %s", ase.getStatusCode()));
       log.error(String.format("AWS Error Code:   %s", ase.getErrorCode()));
       log.error(String.format("Error Type:       %s", ase.getErrorType()));
       log.error(String.format("Request ID:       %s", ase.getRequestId()));
-      throw new AwsException("Failed to upload file!");
+      throw new AwsException(FAILED_TO_UPLOAD_FILE);
     } catch (final AmazonClientException ace) {
-      log.error(
-          "Caught an AmazonClientException, which means the client encountered an internal error"
-              + " while trying to communicate with S3, "
-              + "such as not being able to access the network.");
-      log.error(String.format("Error Message: %s", ace.getMessage()));
-      throw new AwsException("Failed to upload file!");
+      logAmazonClientException(ace);
+      throw new AwsException(FAILED_TO_UPLOAD_FILE);
     }
 
     deleteFile(file);
@@ -150,7 +151,7 @@ public class AwsHelper {
   public String moveFileFromTemp(final String path) {
     final AmazonS3 amazonS3 = findClient();
     try {
-      final String key = path.replace(tempUrl, uploads);
+      final String key = path.replace(TEMP_URL, UPLOADS);
       log.info("move file from /temp");
       amazonS3.copyObject(new CopyObjectRequest(bucketName, path, bucketName, key));
       amazonS3.deleteObject(bucketName, path);
@@ -159,22 +160,21 @@ public class AwsHelper {
       log.error(
           "Caught an AmazonServiceException, which means your request made it to Amazon S3, but was"
               + " rejected with an error response for some reason.");
-      log.error(String.format("Error Message:    %s", ase.getMessage()));
+      log.error(String.format(ERROR_MESSAGE_FORMAT, ase.getMessage()));
       throw new AwsException(ase.getMessage());
     } catch (final AmazonClientException ace) {
-      log.error(
-          "Caught an AmazonClientException, which means the client encountered an internal error "
-              + "while trying to communicate with S3, "
-              + "such as not being able to access the network.");
-      log.error(String.format("Error Message: %s", ace.getMessage()));
+      logAmazonClientException(ace);
       throw new AwsException(ace.getMessage());
     }
   }
 
   public void deleteFile(final File... files) {
     for (final File file : files) {
-      if (file.exists()) {
-        file.delete();
+      try {
+        Files.deleteIfExists(file.toPath());
+      } catch (final IOException e) {
+        log.error(String.format("Caught an IOException: %s", e.getMessage()));
+        throw new AwsException(e.getMessage());
       }
     }
   }
@@ -188,14 +188,10 @@ public class AwsHelper {
       log.error(
           "Caught an AmazonServiceException, which means your request made it to Amazon S3, but "
               + "was rejected with an error response for some reason.");
-      log.error(String.format("Error Message:    %s", ase.getMessage()));
+      log.error(String.format(ERROR_MESSAGE_FORMAT, ase.getMessage()));
       throw new AwsException(ase.getMessage());
     } catch (final AmazonClientException ace) {
-      log.error(
-          "Caught an AmazonClientException, which means the client encountered an internal error "
-              + "while trying to communicate with S3, "
-              + "such as not being able to access the network.");
-      log.error(String.format("Error Message: %s", ace.getMessage()));
+      logAmazonClientException(ace);
       throw new AwsException(ace.getMessage());
     }
   }
@@ -206,7 +202,7 @@ public class AwsHelper {
 
       // Set the pre-signed URL to expire after one hour.
       final java.util.Date expiration = new java.util.Date();
-      final long expTimeMillis = expiration.getTime() + expires;
+      final long expTimeMillis = expiration.getTime() + EXPIRES;
       expiration.setTime(expTimeMillis);
 
       // Generate the pre-signed URL.
@@ -217,15 +213,19 @@ public class AwsHelper {
       final URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
 
       return url.toString();
-    } catch (final AmazonServiceException e) {
+    } catch (final SdkClientException e) {
       // The call was transmitted successfully, but Amazon S3 couldn't process
       // it, so it returned an error response.
       throw new AwsException("Failed to get url of file!", ErrorType.AWS_GET_URL_EXCEPTION, e);
-    } catch (final SdkClientException e) {
-      // Amazon S3 couldn't be contacted for a response, or the client
-      // couldn't parse the response from Amazon S3.
-      throw new AwsException("Failed to get url of file!", ErrorType.AWS_GET_URL_EXCEPTION, e);
     }
+  }
+
+  private void logAmazonClientException(final AmazonClientException e) {
+    log.error(
+        "Caught an AmazonClientException, which means the client encountered an internal error "
+            + "while trying to communicate with S3, "
+            + "such as not being able to access the network.");
+    log.error(String.format("Error Message: %s", e.getMessage()));
   }
 
   public String findAwsPath() {
