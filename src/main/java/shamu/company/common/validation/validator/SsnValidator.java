8c98220573c9153f9c1b5f64719a1b4da08bf283
Wenhao Ji <48240889+wenhao-eb@users.noreply.github.com>
2020-02-26 16:07:36 +0800

    1175: Fix SsnValidator sonarqube issues #746
    1175: Fix SsnValidator sonarqube issues #746

--- src/main/java/shamu/company/common/validation/validator/SsnValidator.java
+++ src/main/java/shamu/company/common/validation/validator/SsnValidator.java
@@ -10,11 +10,6 @@ import shamu.company.common.validation.constraints.SsnValidate;
 public class SsnValidator implements ConstraintValidator<SsnValidate, String> {
 
   @Override
-  public void initialize(SsnValidate constraintAnnotation) {
-
-  }
-
-  @Override
   public boolean isValid(String ssn, ConstraintValidatorContext constraintValidatorContext) {
     if (StringUtils.isEmpty(ssn)) {
       return true;
@@ -23,8 +18,6 @@ public class SsnValidator implements ConstraintValidator<SsnValidate, String> {
     String regex = "^(?!00)(?!666)(?!9[0-9][0-9])\\d{3}[- ]?(?!00)\\d{2}[- ]?(?!0000)\\d{4}$";
     Pattern pattern = Pattern.compile(regex);
     Matcher matcher = pattern.matcher(ssn);
-    boolean result = matcher.matches();
-
-    return result;
+    return matcher.matches();
   }
 }
