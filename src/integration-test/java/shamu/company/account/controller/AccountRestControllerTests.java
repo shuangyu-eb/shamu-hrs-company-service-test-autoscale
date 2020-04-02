d7373cd50c165e009ad627cc8d4799f856dd081d
Jay Hsu <61449098+jay-eb@users.noreply.github.com>
2020-04-02 11:41:19 +0800

    1841: add account controller unit tests in company service (#810)
    1841: add account controller unit tests in company service (#810)

---
+++ src/integration-test/java/shamu/company/account/controller/AccountRestControllerTests.java
@@ -0,0 +1,98 @@
+package shamu.company.account.controller;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import shamu.company.WebControllerBaseTests;
+import shamu.company.helpers.auth0.Auth0Helper;
+import shamu.company.tests.utils.JwtUtil;
+import shamu.company.user.dto.CreatePasswordDto;
+import shamu.company.user.dto.UserLoginDto;
+import shamu.company.utils.JsonUtil;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+
+@WebMvcTest(controllers = AccountRestController.class)
+public class AccountRestControllerTests extends WebControllerBaseTests {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private Auth0Helper auth0Helper;
+
+  @Test
+  void testCreatePasswordTokenExist() throws Exception {
+    final HttpHeaders httpHeaders = new HttpHeaders();
+    final MvcResult response = mockMvc.perform(MockMvcRequestBuilders
+        .get("/company/account/password/1")
+        .headers(httpHeaders)).andReturn();
+    assertThat(response.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+  }
+
+  @Test
+  void testCreatePassword() throws Exception {
+    final HttpHeaders httpHeaders = new HttpHeaders();
+    final CreatePasswordDto createPasswordDto = new CreatePasswordDto();
+    createPasswordDto.setEmailWork("example@example.com");
+    createPasswordDto.setNewPassword("856723Xy");
+    createPasswordDto.setResetPasswordToken("1");
+    final MvcResult response = mockMvc.perform(MockMvcRequestBuilders
+        .patch("/company/account/password")
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(httpHeaders)
+        .content(JsonUtil.formatToString(createPasswordDto))).andReturn();
+    assertThat(response.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+  }
+
+  @Test
+  void testCreatePasswordAndInvitationTokenExist() throws Exception {
+    final HttpHeaders httpHeaders = new HttpHeaders();
+    given(userService.createPasswordAndInvitationTokenExist("1","2")).willReturn(true);
+    final MvcResult response = mockMvc.perform(MockMvcRequestBuilders
+        .get("/company/account/password/1/2")
+        .headers(httpHeaders)).andReturn();
+    assertThat(response.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+  }
+
+  @Test
+  void testUnlock() throws Exception {
+    final HttpHeaders httpHeaders = new HttpHeaders();
+    final UserLoginDto userLoginDto = new UserLoginDto();
+    userLoginDto.setEmailWork("example@example.com");
+    userLoginDto.setPassword("1");
+    final MvcResult response = mockMvc.perform(MockMvcRequestBuilders
+        .patch("/company/account/unlock")
+        .contentType(MediaType.APPLICATION_JSON)
+        .headers(httpHeaders)
+        .content(JsonUtil.formatToString(userLoginDto))).andReturn();
+    assertThat(response.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+  }
+
+  @Test
+  void testValidateChangeWorkEmail() throws Exception {
+    final HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.set("Authorization", "Bearer " + JwtUtil.generateRsaToken());
+    given(userService.changeWorkEmailTokenExist("1")).willReturn(true);
+    final MvcResult response = mockMvc.perform(MockMvcRequestBuilders
+        .patch("/company/account/change-work-email/1")
+        .headers(httpHeaders)).andReturn();
+    assertThat(response.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+  }
+
+  @Test
+  void testResendVerificationEmail() throws Exception {
+    final HttpHeaders httpHeaders = new HttpHeaders();
+    final MvcResult response = mockMvc.perform(MockMvcRequestBuilders
+        .post("/company/account/1/verification-email")
+        .headers(httpHeaders)).andReturn();
+    assertThat(response.getResponse().getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+  }
+}
