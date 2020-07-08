6f5ebf9e5446ee800e3141ccb97c969042044f84
carterr-indeed <53317239+carterr-indeed@users.noreply.github.com>
2020-07-08 16:25:41 -0500

    2389: log request metadata (#1002)
    * 2389: add metadata to request logging
    
    * fix spacing
    
    * resolve sonarqube issues
    
    * clean up code

--- src/main/java/shamu/company/common/config/RequestLoggingFilterConfig.java
+++ src/main/java/shamu/company/common/config/RequestLoggingFilterConfig.java
@@ -1,22 +1,28 @@
 package shamu.company.common.config;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import javax.servlet.Filter;
 import javax.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.ObjectMessage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.filter.AbstractRequestLoggingFilter;
 
 @Configuration
 public class RequestLoggingFilterConfig {
 
-  private static final Logger log = LoggerFactory.getLogger(RequestLoggingFilterConfig.class);
+  private static final Logger log = LogManager.getLogger(RequestLoggingFilterConfig.class);
 
   private static final int MAX_PAYLOAD_LENGTH = 10000;
 
   @Bean
-  public Filter loggingFilter() {
+  public Filter loggingFilter(final Environment env) {
     final AbstractRequestLoggingFilter filter =
         new AbstractRequestLoggingFilter() {
 
@@ -28,7 +34,21 @@ public class RequestLoggingFilterConfig {
           @Override
           protected void afterRequest(final HttpServletRequest request, final String message) {
             if (!request.getRequestURI().contains("/health")) {
-              log.info("{} {}", request.getMethod(), request.getRequestURI());
+              final List profiles = Arrays.asList(env.getActiveProfiles());
+              if (profiles.contains("local")) {
+                  log.info("{} {}", request.getMethod(), request.getRequestURI());
+                  return;
+              }
+              final Map<String, String> map = new TreeMap<>();
+              map.put("HTTP Endpoint", request.getRequestURI());
+              map.put("HTTP Method", request.getMethod());
+              map.put("IP", request.getHeader("X-Forwarded-For"));
+              map.put("Log Type", "Request");
+              map.put("Referer", request.getHeader("Referer"));
+              map.put("User Agent", request.getHeader("User-Agent"));
+              map.put("User Identifier", request.getRemoteUser());
+              final ObjectMessage msg = new ObjectMessage(map);
+              log.info(msg);
             }
           }
         };
