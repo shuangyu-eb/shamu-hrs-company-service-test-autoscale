cb3a11c6774b01af14811c041566dbbdaf1b487d
carterr-indeed <53317239+carterr-indeed@users.noreply.github.com>
2020-07-14 10:40:33 -0500

    2389: add correlation id to thread context for log4j2 (#1030)
    * 2389: add correlation id to thread context for log4j2
    
    * revert compact flag

--- src/main/java/shamu/company/common/config/RequestLoggingFilterConfig.java
+++ src/main/java/shamu/company/common/config/RequestLoggingFilterConfig.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.UUID;
 import javax.servlet.Filter;
 import javax.servlet.http.HttpServletRequest;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang.StringUtils;
+import org.apache.logging.log4j.ThreadContext;
 import org.apache.logging.log4j.message.ObjectMessage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +17,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.filter.AbstractRequestLoggingFilter;
 
 @Configuration
+@Log4j2
 public class RequestLoggingFilterConfig {
-
-  private static final Logger log = LogManager.getLogger(RequestLoggingFilterConfig.class);
 
   private static final int MAX_PAYLOAD_LENGTH = 10000;
 
@@ -26,27 +27,48 @@ public class RequestLoggingFilterConfig {
     final AbstractRequestLoggingFilter filter =
         new AbstractRequestLoggingFilter() {
 
+          private static final String CORRELATION_ID_HEADER_NAME = "X-HRIS-API-Correlation-Id";
+          private static final String CORRELATION_ID_LOG_VAR_NAME = "correlationId";
+
           @Override
           protected void beforeRequest(final HttpServletRequest request, final String message) {
-            // do nothing
+            final String correlationId = getCorrelationIdFromHeader(request);
+            ThreadContext.put(CORRELATION_ID_LOG_VAR_NAME, correlationId);
+            logRequest(request);
           }
 
           @Override
           protected void afterRequest(final HttpServletRequest request, final String message) {
+            ThreadContext.remove(CORRELATION_ID_LOG_VAR_NAME);
+          }
+
+          private String getCorrelationIdFromHeader(final HttpServletRequest request) {
+            String correlationId = request.getHeader(CORRELATION_ID_HEADER_NAME);
+            if (StringUtils.isBlank(correlationId)) {
+              correlationId = generateUniqueCorrelationId();
+            }
+            return correlationId;
+          }
+
+          private String generateUniqueCorrelationId() {
+            return UUID.randomUUID().toString();
+          }
+
+          private void logRequest(final HttpServletRequest request) {
             if (!request.getRequestURI().contains("/health")) {
               final List profiles = Arrays.asList(env.getActiveProfiles());
               if (profiles.contains("local")) {
-                  log.info("{} {}", request.getMethod(), request.getRequestURI());
-                  return;
+                log.info("{} {}", request.getMethod(), request.getRequestURI());
+                return;
               }
               final Map<String, String> map = new TreeMap<>();
-              map.put("HTTP Endpoint", request.getRequestURI());
-              map.put("HTTP Method", request.getMethod());
-              map.put("IP", request.getHeader("X-Forwarded-For"));
-              map.put("Log Type", "Request");
-              map.put("Referer", request.getHeader("Referer"));
-              map.put("User Agent", request.getHeader("User-Agent"));
-              map.put("User Identifier", request.getRemoteUser());
+              map.put("httpEndpoint", request.getRequestURI());
+              map.put("httpMethod", request.getMethod());
+              map.put("userIP", request.getHeader("X-Forwarded-For"));
+              map.put("logType", "Request");
+              map.put("referer", request.getHeader("Referer"));
+              map.put("userAgent", request.getHeader("User-Agent"));
+              map.put("userIdentifier", request.getRemoteUser());
               final ObjectMessage msg = new ObjectMessage(map);
               log.info(msg);
             }
