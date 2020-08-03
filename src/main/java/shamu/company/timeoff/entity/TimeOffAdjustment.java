9714c12913d8c65e497e8d018de07c1c3c09e520
xujiansheng-eb <62226440+xujiansheng-eb@users.noreply.github.com>
2020-08-03 16:32:15 +0800

    2786: Add Column update_at To time_off_adjustments (#1114)

--- src/main/java/shamu/company/timeoff/entity/TimeOffAdjustment.java
+++ src/main/java/shamu/company/timeoff/entity/TimeOffAdjustment.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
+import shamu.company.common.entity.BaseEntity;
 import shamu.company.company.entity.Company;
 import shamu.company.user.entity.User;
 
@@ -23,7 +24,7 @@ import shamu.company.user.entity.User;
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
-public class TimeOffAdjustment {
+public class TimeOffAdjustment extends BaseEntity {
 
   @Id
   @GeneratedValue(generator = "UUID")
